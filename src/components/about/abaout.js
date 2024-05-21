364d9a826030b51edc1ef9e153818c1955ad57a5
hvicil <100441731+hvicil@users.noreply.github.com>
2024-05-21 19:49:01 +0200

    21052024

--- src/components/about/abaout.js
+++ src/components/about/abaout.js
@@ -8,6 +8,7 @@ const About = () => {
     const endValue = 100; // Son değer (%100)
     const startYear = 2024; // Başlangıç yılı
     const currentYear = new Date().getFullYear(); // Mevcut yıl
+    const currentHour = new Date().getHours(); // Saat bilgisini alır
     const totalYears = 80; // Toplam yıl
 
     const valueIncrease = endValue - startValue;
@@ -18,8 +19,11 @@ const About = () => {
     }
 
     const currentValue =
-      startValue + (valueIncrease * yearProgress) / totalYears;
-    return currentValue.toFixed(1); // Virgülden sonra 2 basamak
+      startValue +
+      (valueIncrease * yearProgress) / totalYears +
+      ((24 - currentHour) / 24) * 0.3 +
+      0.1;
+    return currentValue.toFixed(1); // Virgülden sonra 1 basamak
   };
 
   return (
